20166c39fb652cf37a4e6080677afebc5e1f6b91
brian <brayan_andrey23@hotmail.com>
2020-04-01 18:36:38 -0500

    se crea validacion de campo password en form de inicio de sesion

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
+//regex para validar campos
 const regexEmail = /\w+@\w+\.+[a-zA-Z]/;
+const regexPass = /\w+/;
 
+//botones y inputs de formulario
 let botonInicioSesion = document.getElementById('btn-iniciarsesion');
 let campoEmail = document.getElementById('formGroupExampleInput');
+let campoPassword = document.getElementById('formGroupExampleInput2');
 
 //funcion para validar un correo
 function validacion() {
@@ -12,6 +16,17 @@ function validacion() {
     }
 }
 
+//funcion para validar un password en inicio de sesion
+function validacionPass() {
+    if(!regexPass.test(campoPassword.value)){
+        campoPassword.style.border='2px solid red';
+    }else{
+        campoPassword.style.border='1px solid #ced4da';
+    }
+}
+
+//validacion de campos inicio de sesión
 botonInicioSesion.addEventListener("click",()=>{
     validacion();
+    validacionPass();
 });
