347ab6e0948e899f0c60b3ead8cc447f37fbdb1c
brian <brayan_andrey23@hotmail.com>
2020-04-27 21:22:14 -0500

    realizando la vista proveedores

--- vistaAdministrador/js/principal-administrador.js
+++ vistaAdministrador/js/principal-administrador.js
@@ -1,12 +1,35 @@
-let botonVentas = document.getElementById('mod-ventas');
-let subMenu = document.getElementById('sub-menu-ventas');
+const botonVentas = document.getElementById('menu-principal-ventas');
+const subMenuVentas = document.getElementById('sub-menu-ventas');
+const botonInventarios = document.getElementById('mod-inventarios');
+const subMenuInventarios = document.getElementById('sub-menu-inventarios');
+const botonFide = document.getElementById('mod-fidelizacion');
+const subMenuFidelizacion = document.getElementById('sub-menu-fidelizacion');
+const botonUsuarios = document.getElementById('mod-usuarios')
+const subMenuUsuarios = document.getElementById('sub-menu-usuarios');
+const botonReportes = document.getElementById('mod-reportes');
+const subMenuReportes = document.getElementById('sub-menu-reportes');
 
-botonVentas.addEventListener('click',function () {
-    if(subMenu.classList.contains('mostrar')){
-        subMenu.classList.add('ocultar');
-        subMenu.classList.remove('mostrar');
-    }else{
-        subMenu.classList.add('mostrar');
-        subMenu.classList.remove('ocultar');
-    }
+
+function mostrar(elemento) {
+    elemento.classList.toggle('mostrar')
+}
+
+botonVentas.addEventListener('click',()=>{
+    mostrar(subMenuVentas);
+});
+
+botonInventarios.addEventListener('click',()=>{
+    mostrar(subMenuInventarios);
+});
+
+botonFide.addEventListener('click',()=>{
+    mostrar(subMenuFidelizacion);
+});
+
+botonUsuarios.addEventListener('click',()=>{
+    mostrar(subMenuUsuarios);
+});
+
+botonReportes.addEventListener('click',()=>{
+    mostrar(subMenuReportes);
 });
