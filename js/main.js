f72c8ad87f0e4d12acbcda271ee02cc88edde014
brian <brayan_andrey23@hotmail.com>
2020-06-08 21:00:33 -0500

    añadiendo validacion de campos de formulario con texto

--- js/main.js
+++ js/main.js
@@ -6,13 +6,16 @@ const regexPass = /\w{5,15}/;
 let botonInicioSesion = document.getElementById('btn-iniciarsesion');
 let campoEmail = document.getElementById('formGroupExampleInput');
 let campoPassword = document.getElementById('formGroupExampleInput2');
-
+let mensajeError = document.getElementsByClassName("ocultar");
+console.log(mensajeError);
 //funcion para validar un correo
 function validacion() {
     if(!regexEmail.test(campoEmail.value)){
         campoEmail.style.border='2px solid red';
+        mensajeError[0].classList.add("mostrarmsg");
     }else{
         campoEmail.style.border='1px solid #ced4da';
+        mensajeError[0].classList.remove("mostrarmsg");
     }
 }
 
@@ -20,8 +23,10 @@ function validacion() {
 function validacionPass() {
     if(!regexPass.test(campoPassword.value)){
         campoPassword.style.border='2px solid red';
+        mensajeError[1].classList.add("mostrarmsg");
     }else{
         campoPassword.style.border='1px solid #ced4da';
+        mensajeError[1].classList.remove("mostrarmsg");
     }
 }
 
