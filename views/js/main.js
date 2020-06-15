a8a0755acbe21480e0d152b5552ece6f8c680522
brian <brayan_andrey23@hotmail.com>
2020-06-15 13:36:36 -0500

    creando la interfaz de proveedores y corrigiendo bugs de cierre de sesion

--- views/js/main.js
+++ views/js/main.js
@@ -1,43 +1,60 @@
 //regex para validar campos
 const regexEmail = /\w+@\w+\.+[a-zA-Z]/;
-const regexPass = /\w{5,15}/;
+const regexfiveTofifteencharacters = /\w{5,15}/;
+const numeroCelular = /[0-9]{10,10}/
 
+
+//FORMULARIO INCIO SESIÓN
 //botones y inputs de formulario
 let botonInicioSesion = document.getElementById('btn-iniciarsesion');
-let campoEmail = document.getElementById('formGroupExampleInput');
-let campoPassword = document.getElementById('formGroupExampleInput2');
+let campoEmail = document.getElementById('correoElectronico');
+let campoPassword = document.getElementById('contrasena');
 let mensajeError = document.getElementsByClassName("ocultar");
-
-
+const formularioInicioSesion = document.getElementById('formlogin');
 
 //funcion para validar un correo
-function validacion() {
+function validacionEmail(e) {
     if(!regexEmail.test(campoEmail.value)){
-        campoEmail.style.border='2px solid red';
+        campoEmail.style.borderBottom ='1px solid red';
         mensajeError[0].classList.add("mostrarmsg");
-        return false;
+        e.preventDefault();
     }else{
-        campoEmail.style.border='1px solid #ced4da';
+        campoEmail.style.borderBottom ='';
         mensajeError[0].classList.remove("mostrarmsg");
-        return true;
     }
 }
-
-//funcion para validar un password en inicio de sesion
-function validacionPass() {
-    if(!regexPass.test(campoPassword.value)){
-        campoPassword.style.border='2px solid red';
+//funcion para validar la contraseña
+function validarPass(e) {
+    if(!regexfiveTofifteencharacters.test(campoPassword.value)){
+        campoPassword.style.borderBottom = '1px solid red'
         mensajeError[1].classList.add("mostrarmsg");
-        return false;
+        e.preventDefault();
     }else{
-        campoPassword.style.border='1px solid #ced4da';
+        campoPassword.style.borderBottom=''
         mensajeError[1].classList.remove("mostrarmsg");
-        return true;
     }
 }
+//validacion inicio sesión
+function validacionGeneral(e) {
+    validacionEmail(e);
+    validarPass(e);
+    redireccionar();
+}
+formularioInicioSesion.addEventListener('submit',validacionGeneral);
+//FORMULARIO INCIO SESIÓN
 
-//validacion de campos inicio de sesión
-botonInicioSesion.addEventListener("click",()=>{
-    validacion();
-    validacionPass();
-});
+//REDIRECCIONAR VISTAS
+const userAdmin = 'pepeMojica@hotmail.com';
+const passAdmin = 'pepe123';
+const userCustomer = 'briolarte@gmail.com';
+const passCustomer = 'bri123';
+function redireccionar() {
+    if(campoEmail.value === userAdmin && campoPassword.value === passAdmin){
+        window.open('./views/vistasporusuarios/vista-administrador/reportes.html')
+    }else if(campoEmail.value === userCustomer && campoPassword.value === passCustomer){
+        window.open('./views/vistasporusuarios/vista-cliente/pedidos-cliente.html')
+    }   
+}
+
+
+
---
+++ views/vistasporusuarios/js/graphs.js
@@ -0,0 +1,70 @@
+const GRAPH_SELLS = document.getElementById('grafica-ventas').getContext('2d')
+const GRAPH_PURCHASES = document.getElementById('grafica-compras')
+
+let chartSells = new Chart(GRAPH_SELLS,{
+    type:'bar',
+    data:{
+        labels:['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre'],
+        datasets:[{
+            label: 'SILEG VENTAS MES A MES',
+            data:[200000,300000,280000,290000,150000,350000,450000,650000,350000,350000,650000,1250000],
+            backgroundColor:[
+                'rgb(105, 209, 117)',
+                'rgb(105, 209, 117)',
+                'rgb(105, 209, 117)',
+                'rgb(105, 209, 117)',
+                'rgb(105, 209, 117)',
+                'rgb(105, 209, 117)',
+                'rgb(105, 209, 117)',
+                'rgb(105, 209, 117)',
+                'rgb(105, 209, 117)',
+                'rgb(105, 209, 117)',
+                'rgb(105, 209, 117)',
+                'rgb(105, 209, 117)'
+            ]
+        }]
+    },
+    options:{
+        scales:{
+            yAxes:[{
+                ticks:{
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+})
+
+let chartPurchases =  new Chart(GRAPH_PURCHASES,{
+    type:'bar',
+    data:{
+        labels:['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre'],
+        datasets:[{
+            label: 'SILEG COMPRAS MES A MES',
+            data:[200000,300000,280000,290000,150000,350000,450000,650000,350000,350000,650000,1250000],
+            backgroundColor:[
+                'rgb(212, 204, 66)',
+                'rgb(212, 204, 66)',
+                'rgb(212, 204, 66)',
+                'rgb(212, 204, 66)',
+                'rgb(212, 204, 66)',
+                'rgb(212, 204, 66)',
+                'rgb(212, 204, 66)',
+                'rgb(212, 204, 66)',
+                'rgb(212, 204, 66)',
+                'rgb(212, 204, 66)',
+                'rgb(212, 204, 66)',
+                'rgb(212, 204, 66)'
+            ]
+        }]
+    },
+    options:{
+        scales:{
+            yAxes:[{
+                ticks:{
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+})
